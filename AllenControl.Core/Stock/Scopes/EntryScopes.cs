e404ee18d3d989f4a267db4e050e0d1d26e51fbe
Kevin Allen <kevinallen@outlook.com>
2015-11-04 21:59:58 -0300

    Escopo de registro de entrada.

--- AllenControl.Core/Stock/Scopes/EntryScopes.cs
+++ AllenControl.Core/Stock/Scopes/EntryScopes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using AllenControl.Core.Stock.Entities;
 using AllenControl.Core.Stock.Enums;
+using DomainNotificationHelper.Validation;
 using static DomainNotificationHelper.Validation.AssertionConcern;
 
 namespace AllenControl.Core.Stock.Scopes
@@ -16,7 +17,12 @@ namespace AllenControl.Core.Stock.Scopes
 
         public static bool RegisterScopeIsValid(this Entry entry)
         {
-            throw new NotImplementedException();
+            return IsSatisfiedBy(
+                AssertNotEmpty(entry.ProductId, "O produto é obrigatório"),
+                AssertIsGreaterThan(entry.Amount, 0, "A quantidade deve ser maior que zero."),
+                AssertIsGreaterThan(entry.Price, 0, "O preço deve ser maior que zero.")
+
+                );
         }
     }
 }
